92e37ab2c3721d6f6d2e858c131d8ccfa7cffd97
Tim Mace <timothy.mace@outlook.com>
2021-07-06 08:59:49 -0400

    hello world test

---
+++ functions/v1-move.js
@@ -0,0 +1,18 @@
+// Create a handler to capture event body of incoming request
+exports.handler = async (event) => {
+  try {
+    // Only allow POST requests
+    if (event.httpMethod !== "POST") {
+      return { statusCode: 405, body: "HTTP method not allowed" };
+    }
+    // Parse the event body
+    const body = JSON.parse(event.body);
+    console.log("Received request:", body);
+    // Return success
+    return { statusCode: 200, body: "Hello world" };
+  } catch (err) {
+    // Catch unexpected errors within try block and return failure
+    console.error("Encountered an unexpected error:", err);
+    return { statusCode: 500, body: "Unexpected error handling request. Please try again." };
+  }
+};
