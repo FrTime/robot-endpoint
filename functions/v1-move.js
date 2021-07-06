c2280c88169c21c7eab7eafd74617d02a46df657
Tim Mace <timothy.mace@outlook.com>
2021-07-06 12:59:18 -0400

    Using sdk function with added distance range param

--- functions/v1-move.js
+++ functions/v1-move.js
@@ -12,7 +12,7 @@ exports.handler = async (event) => {
     if (!body.loadId || !body.x || !body.y) {
       return { statusCode: 400, body: "Request is missing one or more parameters" };
     }
-    let robot = await findRobot(body.x, body.y);
+    let robot = await sdk.findRobot(body.x, body.y, 10);
     if (!robot) {
       return { statusCode: 204, body: "No available robots to fulfil request" };
     }
@@ -24,20 +24,4 @@ exports.handler = async (event) => {
     console.error("Encountered an unexpected error:", err);
     return { statusCode: 500, body: "Unexpected error handling request. Please try again." };
   }
-};
-
-const findRobot = async (x, y) => {
-  // fetch list of available robots
-  let robots = await sdk.fetchAvailableRobots();
-  // Enrich the list of robots with the distance to the request's xy-coordinate
-  robots = sdk.enrichRobotList(robots, x, y);
-  // Sort the list of enriched robots by closest distance to the xy-coordinate
-  sdk.sortByDistance(robots);
-  // Check for robots available within 10 units of distance
-  let closestRobots = sdk.filterByDistance(robots, 10);
-  console.log("closest robots:", closestRobots);
-  // If there are multiple robots within 10 units of distance, return the robot with the most battery
-  if (!closestRobots || closestRobots.length > 1) return sdk.sortByBattery(closestRobots)[0];
-  // Otherwise, return the closest robot
-  else return closestRobots[0];
 };
